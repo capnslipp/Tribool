a373e484db2f074c03b8dadead4e59314de90316
Slipp Douglas Thompson <git@capnslipp.com>
2019-04-29 20:30:21 -0500

    Swift 3 build overlay
    * Changed the xcodeproj's Swift Version build setting to 3 (`3.0`).
    * Changed `.swift-version` to contain “3.0”.
    * Changed `Package.swift`'s initial line to `// swift-tools-version:3.0`.
    * Changed `Package.swift`'s `swiftLanguageVersions:` to include only integer values.
    * Removed `Package.swift`'s `products:` entry (not supported by Swift 3).
    * Changed `Package.swift`'s `.target(…)` entry to Swift 3's `Target(…)` style.
    * Removed `Package.swift`'s `Target(…)`'s `path:` argument (not supported by Swift 3).

--- Package.swift
+++ Package.swift
@@ -1,19 +1,15 @@
-// swift-tools-version:5.0
+// swift-tools-version:3.0
 import PackageDescription
 
 let package = Package(
 	name: "Tribool",
-	products: [
-		.library(name: "Tribool", targets: [ "Tribool" ]),
+	targets: [
+		Target(name: "Tribool", dependencies: []),
 	],
 	dependencies: [],
-	targets: [
-		.target(name: "Tribool", dependencies: [], path: "Sources/"),
-	],
 	swiftLanguageVersions: [
-		.version("3"),
-		.version("4"),
-		.version("4.2"),
-		.version("5"),
+		3,
+		4,
+		5,
 	]
 )
