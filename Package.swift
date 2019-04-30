3991eb56919f8ebf33a99bc9cbadff02cc371bed
Slipp Douglas Thompson <git@capnslipp.com>
2019-04-29 20:00:30 -0500

    Updated for SPM
    * Added all the additional modern fields to `Package.swift`— `products:`, `dependencies:`, `targets:`, and `swiftLanguageVersions:`.  
    	▸ I added all the Swift versions that the code will compile under — 3, 4, 4.2, & 5 — as `.version("…")` enum entries (instead of using the built-in `.v4`, etc. entries) because the package spec doesn't otherwise allow specifying newer-than-current entries, which is necessary when overlaying this package file for older Swift versions.

--- Package.swift
+++ Package.swift
@@ -2,5 +2,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "Tribool"
+	name: "Tribool",
+	products: [
+		.library(name: "Tribool", targets: [ "Tribool" ]),
+	],
+	dependencies: [],
+	targets: [
+		.target(name: "Tribool", dependencies: [], path: "Sources/"),
+	],
+	swiftLanguageVersions: [
+		.version("3"),
+		.version("4"),
+		.version("4.2"),
+		.version("5"),
+	]
 )
